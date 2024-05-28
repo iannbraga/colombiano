01787f16641b738a5be86f3d83a63e0f9d79a76e
iann.braga <bragaprestes@gmail.com>
2024-05-28 13:14:21 -0300

    Ajuste no script

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 
         for (let categoria in produtosPorCategoria) {
             let categoriaNavItem = `
-            <a class="nav-link text-white fs-5 font-monospace" href="#${categoria.replace(/\s+/g, '-')}">
+            <a class="nav-link text-dark fs-5 font-monospace" href="#${categoria.replace(/\s+/g, '-')}">
             ${categoria}
             </a>`
             categoriasProdutos.append(categoriaNavItem);
@@ -34,23 +34,24 @@ $(document).ready(function () {
             <li><a class="dropdown-item" href="#${categoria.replace(/\s+/g, '-')}">${categoria}</a></li>`
             categoriasProdutosFloatingBtn.append(categoriaFloatItem);
 
-
             let categoriaDiv = $('<div id="' + categoria.replace(/\s+/g, '-') + '" class="col-12"></div>');
             
             let categoriaHeader = $('<h2 class="my-4 text-center display-6"></h2>').text(categoria);
             categoriaDiv.append(categoriaHeader);
-            categoriaDiv.append(`<hr class="p-3">`)
 
             let produtosRow = $('<div class="row pt-3"></div>');
 
             produtosPorCategoria[categoria].forEach(function (produto) {
                 let produtoCard = `
                     <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-                        <div class="card h-100">
+                        <div class="card" style="height: 350px;">
                             <div class="card-body text-center">
-                                <i class="${produto.icone} fa-3x mb-3"></i>
-                                <h5 class="card-title">${produto.nome}</h5>
-                                <p class="card-text">R$ ${produto.preco}</p>
+                                ${produto.image ? 
+                                    `<img src="${produto.image}" class="img-fluid mb-${produto.nome == 'Redbull' ? '0':'4'} "/>` 
+                                    : 
+                                    `<i class="${produto.icone} fa-3x mb-3 mt-5 pt-5"></i>`}
+                                <h6 class="card-title ${produto.image ? '' : 'mt-5 pt-2'}">${produto.nome}</h6>
+                                <p class="badge bg-success card-text">R$ ${produto.preco}</p>
                             </div>
                         </div>
                     </div>
